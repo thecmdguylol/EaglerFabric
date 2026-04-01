a919c3a4a361e7561a5ebfb9511fd0af0d7382d0
cmdguy <32jwells@stu.asher.k12.ok.us>
2026-04-01 11:33:24 -0500

    Refactor event injection code and update syntax

--- core/patches.js
+++ core/patches.js
@@ -1,9 +1,18 @@
 class PatchesRegistry {
     static patchFns = []
     static patchedEventNames = []
+
     static getEventInjectorCode() {
-        return "globalThis.modapi_specialevents = [" + PatchesRegistry.patchedEventNames.flatMap(x=>`\`${x}\``).join(",") + "];";
+        return `
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script>
+    globalThis.modapi_specialevents = [
+        ${PatchesRegistry.patchedEventNames.flatMap(x => `'${x}'`).join(',')}
+    ];
+</script>
+`;
     }
+
     static patchFile(x) {
         var current = x;
         PatchesRegistry.patchFns.forEach(fn => {
@@ -11,28 +20,27 @@ class PatchesRegistry {
         });
         return current;
     }
+
     static addPatch(fn) {
         PatchesRegistry.patchFns.push(fn);
     }
+
     static regSpecialEvent(x) {
         PatchesRegistry.patchedEventNames.push(x);
     }
 }
-// PatchesRegistry.regSpecialEvent("test");
+
+// PatchesRegistry.regSpecialEvent('test');
 // PatchesRegistry.addPatch(function (input) {
 //     var output = input;
 //     return output;
 // })
 
-PatchesRegistry.regSpecialEvent("render");
+PatchesRegistry.regSpecialEvent('render');
 PatchesRegistry.addPatch(function (input) {
     var output = input.replaceAll(
-        /continue main;\s+?}\s+?if\s?\(!\$this.\$renderHand\)/gm
-        ,
-`continue main;
-            }
-            ModAPI.events.callEvent("render",{partialTicks:$partialTicks})
-            if (!$this.$renderHand)`
+        /continue main;\s+?}\s+?if\s?\(!\$this.\$renderHand\)/gm,
+        "continue main; } ModAPI.events.callEvent('render',{partialTicks:$partialTicks}); if (!$this.$renderHand)"
     );
     return output;
 });
